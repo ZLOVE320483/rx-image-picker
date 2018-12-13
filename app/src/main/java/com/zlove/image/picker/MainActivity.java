5c439f32997b52bf254849bad73823c0340738f3
zlove <zlove.zhang@bytedance.com>
2018-12-13 13:39:47 +0800

    Create Project

---
+++ app/src/main/java/com/zlove/image/picker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zlove.image.picker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
