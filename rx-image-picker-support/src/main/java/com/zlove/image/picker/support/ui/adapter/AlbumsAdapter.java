cdd3fd4aad4ee8242d8c7ea2113733822fae8849
zlove <zlove.zhang@bytedance.com>
2018-12-21 20:08:06 +0800

    Add Adapter

---
+++ rx-image-picker-support/src/main/java/com/zlove/image/picker/support/ui/adapter/AlbumsAdapter.java
@@ -0,0 +1,61 @@
+package com.zlove.image.picker.support.ui.adapter;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.database.Cursor;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.support.v7.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.zlove.image.picker.support.R;
+import com.zlove.image.picker.support.entity.Album;
+import com.zlove.image.picker.support.entity.SelectionSpec;
+
+import java.io.File;
+
+public class AlbumsAdapter extends CursorAdapter {
+
+    private Drawable mPlaceholder;
+
+    public AlbumsAdapter(Context context, Cursor c, boolean autoRequery) {
+        super(context, c, autoRequery);
+
+        TypedArray ta = context.getTheme().obtainStyledAttributes(new int[]{R.attr.album_thumbnail_placeholder});
+        mPlaceholder = ta.getDrawable(0);
+        ta.recycle();
+    }
+
+    public AlbumsAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+
+        TypedArray ta = context.getTheme().obtainStyledAttributes(new int[]{R.attr.album_thumbnail_placeholder});
+        mPlaceholder = ta.getDrawable(0);
+        ta.recycle();
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        ContextThemeWrapper contextThemeWrapper = new ContextThemeWrapper(context, SelectionSpec.instance.themeId);
+        return LayoutInflater.from(context)
+                .cloneInContext(contextThemeWrapper)
+                .inflate(R.layout.album_list_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        Album album = Album.valueOf(cursor);
+        ((TextView) view.findViewById(R.id.album_name)).setText(album.getDisplayName(context));
+        ((TextView) view.findViewById(R.id.album_media_count)).setText(String.valueOf(album.count));
+
+        SelectionSpec.instance.imageEngine.loadThumbnail(context,
+                context.getResources().getDimensionPixelSize(R.dimen.media_grid_size),
+                mPlaceholder,
+                view.findViewById(R.id.album_cover),
+                Uri.fromFile(new File(album.coverPath)));
+    }
+}
